141a7a0652235df834665d452046506a8191fd64
Rookie0031 <jisu7408@gmail.com>
2023-01-27 00:05:54 +0900

    [Feat] HTTP 관련 코드 추가

---
+++ UIStackViewStudy/UIStackViewStudy/HTTPCase.swift
@@ -0,0 +1,14 @@
+//
+//  HTTPCase.swift
+//  UIStackViewStudy
+//
+//  Created by Jisu Jang on 2023/01/26.
+//
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
